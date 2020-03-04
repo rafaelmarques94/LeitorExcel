4db24762082a7ce14b4cb04a85996790d59f1409
Rafael Marques <rafael-w5@hotmail.com>
2020-03-04 12:32:55 -0300

    Add project files.

---
+++ HelperExcel/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace HelperExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bnt_UploadExcel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // bnt_UploadExcel
+            // 
+            this.bnt_UploadExcel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.bnt_UploadExcel.Image = ((System.Drawing.Image)(resources.GetObject("bnt_UploadExcel.Image")));
+            this.bnt_UploadExcel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bnt_UploadExcel.Location = new System.Drawing.Point(21, 22);
+            this.bnt_UploadExcel.Name = "bnt_UploadExcel";
+            this.bnt_UploadExcel.Size = new System.Drawing.Size(190, 53);
+            this.bnt_UploadExcel.TabIndex = 0;
+            this.bnt_UploadExcel.Text = "    Selecionar Arquivo Excel";
+            this.bnt_UploadExcel.UseVisualStyleBackColor = true;
+            this.bnt_UploadExcel.Click += new System.EventHandler(this.Bnt_UploadExcel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Celulas:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(21, 105);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(190, 186);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 316);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bnt_UploadExcel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Import Excel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bnt_UploadExcel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ HelperExcel/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace HelperExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private DataSet ConexaoPlanilha(string caminho)
+        {
+            DataSet dsPlanilhaImportada = new DataSet();
+
+            OleDbConnection conexao = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + caminho + ";Extended Properties='Excel 12.0;'");
+            conexao.Open();
+
+            DataTable dt = conexao.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+            List<string> listaGuias = new List<string>();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                if (row["TABLE_NAME"].ToString().Contains("$"))
+                {
+                    listaGuias.Add(row["TABLE_NAME"].ToString());
+                }
+            }
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter("SELECT * FROM [" + listaGuias[0] + "]", conexao);
+            adapter.Fill(dsPlanilhaImportada);
+
+            return dsPlanilhaImportada;
+        }
+
+        private void Bnt_UploadExcel_Click(object sender, EventArgs e)
+        {
+            // Limpa os itens do listBox
+            listBox1.Items.Clear();
+
+            OpenFileDialog ofdSelecionarArquivo = new OpenFileDialog();
+
+            if (ofdSelecionarArquivo.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var filePath = ofdSelecionarArquivo.FileName;                
+                    DataSet dsPlanilhaImportada = ConexaoPlanilha(filePath);
+
+                    MessageBox.Show("A planilha foi importada com Sucesso!" + "\n" + "as celulas serão exibidas na lista abaixo", "Planilha Importada", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    for (int i = 0; i < dsPlanilhaImportada.Tables[0].Rows.Count; i++)
+                    {
+                        string ValorCelula = dsPlanilhaImportada.Tables[0].Rows[i][0].ToString();
+
+                        listBox1.Items.Add(ValorCelula);
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erro.\n\nMensagem de Erro: {ex.Message}\n\n");
+
+                }
+            }
+        }
+    }
+}
